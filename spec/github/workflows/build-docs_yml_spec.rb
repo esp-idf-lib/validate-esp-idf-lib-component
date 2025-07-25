033d549e8c23466da21d4f62fe259bdb5a44d16d
Tomoyuki Sakurai <y@trombik.org>
2025-07-25 15:40:19 +0700

    bugfix: skip in the example

--- spec/github/workflows/build-docs_yml_spec.rb
+++ spec/github/workflows/build-docs_yml_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 RSpec.describe ".github/workflows/build-docs.yml" do
   let(:file) { repo / ".github/workflows/build-docs.yml" }
-  skip "repository shared-workflows does not need it" if eil["name"] == "shared-workflows"
 
   it "exists" do
+    skip "repository shared-workflows does not need it" if eil["name"] == "shared-workflows"
     expect(file).to be_file, "expected #{file} to be file, but not. copy #{file.basename} from https://github.com/esp-idf-lib/template-component/tree/main/.github/workflows"
   end
 end
