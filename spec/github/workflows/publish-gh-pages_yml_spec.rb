278251e593caebf18c26b7fa94557cc490159c35
Tomoyuki Sakurai <y@trombik.org>
2025-07-25 15:41:48 +0700

    bugfix: one more skip

--- spec/github/workflows/publish-gh-pages_yml_spec.rb
+++ spec/github/workflows/publish-gh-pages_yml_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ".github/workflows/publish-gh-pages.yml" do
   let(:file) { repo / ".github/workflows/publish-gh-pages.yml" }
 
   it "exists" do
+    skip "repository shared-workflows does not need it" if eil["name"] == "shared-workflows"
     expect(file).to be_file, "expected #{file} to be file, but not. copy #{file.basename} from https://github.com/esp-idf-lib/template-component/tree/main/.github/workflows"
   end
 end
