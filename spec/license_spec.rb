e366dc7e7f11f75c6ec7c55f7635107ba839a04b
Tomoyuki Sakurai <y@trombik.org>
2025-08-01 09:45:48 +0700

    verify LICENSE file exists

---
+++ spec/license_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+RSpec.describe "LICENSE" do
+  let(:file) { repo / "LICENSE" }
+  let(:content) { File.read file }
+
+  it "exists" do
+    expect(file).to exist
+  end
+
+  it "is a file" do
+    expect(file).to be_file
+  end
+
+  it "is readable" do
+    expect(file).to be_readable
+  end
+
+  describe "LICENSE's content" do
+    it "has a line starting with `Copyright (c)`" do
+      expect(content).to match(/Copyright \(c\)/)
+    end
+  end
+end
+
