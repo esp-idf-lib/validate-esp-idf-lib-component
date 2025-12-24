5b21f84f78804722384ffa5786828a2db3cb022f
Tomoyuki Sakurai <y@trombik.org>
2025-12-24 18:23:54 +0700

    doc: document how to switch to eil-cmake-utils

--- spec/eil_cmake_utils_spec.rb
+++ spec/eil_cmake_utils_spec.rb
@@ -4,7 +4,28 @@ RSpec.describe "eil-cmake-utils" do
   let(:dir) { repo / "eil-cmake-utils" }
 
   it "exists" do
-    expect(dir).to exist
+    how_to_fix = <<~TEXT
+    How to fix it:
+      * Remove "common` git submodule
+        * git rm common
+      * Add eil-cmake-utils as a submodule
+        * git submodule add git@github.com:esp-idf-lib/eil-cmake-utils.git
+
+    Edit CMakeLists.txt and add the following at the TOP of file:
+
+      list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/eil-cmake-utils/cmake)
+
+    Edit CMakeLists.txt and add the following at the BOTTOM of file:
+
+      include(eil_ci)
+
+    Check if the build passes
+
+    idf.py build -C examples/default
+
+    Whne the test passes, commit the change.
+    TEXT
+    expect(dir).to exist, how_to_fix
   end
 
   it "is a directory" do
